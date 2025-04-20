fda68a99da0418f7068a1849dfb78a3fc4f1ddc5
Aya Hamadani <ayahamadani2004@gmail.com>
2025-04-21 00:58:17 +0300

    Add a volume range input to control the volume of the songs, and add the UI for controlling playlists.

--- client/src/components/BottomPlayer/BottomPlayer.js
+++ client/src/components/BottomPlayer/BottomPlayer.js
@@ -1,4 +1,4 @@
-import { React, useEffect, useState, useContext } from 'react';
+import { React, useEffect, useState, useContext, useRef } from 'react';
 import SongContext from '../context/SongContext';
 import styles from '../../pages/Home/Home.module.css';
 
@@ -6,6 +6,10 @@ export default function BottomPlayer() {
     const { currentSongData, setCurrentSongData, playSong, currentSongAudio, setCurrentSongAudio, rewindSong, songs, setSongs, getSongIndex, skipSong, toggleLike } = useContext(SongContext);
     const [currentTime, setCurrentTime] = useState(0);
     const [duration, setDuration] = useState(0);
+    const [volume, setVolume] = useState(1);
+    const [lastVolume, setLastVolume] = useState(1);
+    const [showDropdown, setShowDropdown] = useState(false);
+    const dropdownRef = useRef();
 
     useEffect(() => {
         if (!currentSongAudio) return;
@@ -37,6 +41,35 @@ export default function BottomPlayer() {
         return `${minutes}:${seconds}`;
       };
 
+      const toggleVolume = () => {
+        if (volume > 0) {
+          setLastVolume(volume);
+          setVolume(0);
+          currentSongAudio.volume = 0;
+        } else {
+          setVolume(lastVolume);
+          currentSongAudio.volume = lastVolume;
+        }
+      };
+    
+      const changeVolume = (e) => {
+        let value = e.target.value;
+        setVolume(value);
+        currentSongAudio.volume = value;
+      };
+
+    // Hide dropdown if clicking outside
+      useEffect(() => {
+        const handleClickOutside = (e) => {
+          if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+            setShowDropdown(false);
+          }
+        };
+    
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => document.removeEventListener("mousedown", handleClickOutside);
+      }, []);
+
     return (
       <div className={styles.stickyBottom}>
         <div style={{display: "flex", gap: "0.7em"}}>
@@ -66,8 +99,56 @@ export default function BottomPlayer() {
                 <span style={{color: "white"}}>{formatTime(duration)}</span>
             </div>
         </div>
-        <div style={{ paddingRight: "3em"}}>
-            <i className="fa-solid fa-plus" style={{color: "white", marginRight: "1em", cursor: "pointer"}}></i>
+
+         
+        
+        <div style={{ paddingRight: "3em", display: "flex"}}>
+          <div style={{display: "flex"}}>
+              <div style={{marginRight: "0.5em"}}>
+                {volume > 0 && volume < 0.6 ? (
+                  <i
+                    style={{color: "white"}}
+                    className="fa-solid fa-volume-low"
+                    onClick={() => toggleVolume()}
+                  />
+                ) : volume >= 0.6 ? (
+                  <i
+                    style={{color: "white"}}
+                    className="fa-solid fa-volume-high"
+                    onClick={() => toggleVolume()}
+                  />
+                ) : (
+                  <i
+                    style={{color: "white"}}
+                    className="fa-solid fa-volume-off"
+                    onClick={() => toggleVolume()}
+                  />
+                )}
+              </div>
+              <div className="mr1">
+                <input
+                  className={styles.volumeInput}
+                  type="range"
+                  style={{ marginRight: "0.5em"}}
+                  min="0"
+                  max="1"
+                  step="0.1"
+                  value={volume}
+                  onChange={changeVolume}
+                />
+              </div>
+            </div>
+            {showDropdown && (
+              <div className={styles.bottomDropDownMenu}>
+                <div className={styles.dropdownItem}>Add To Existing Playlist</div>                
+                <div className={styles.dropdownItem} style={{color: "purple"}}>Create A New Playlist</div>
+              </div>
+            )}
+            <i
+              className="fa-solid fa-plus"
+              style={{color: "white", marginRight: "1em", cursor: "pointer"}}
+              onClick={() => setShowDropdown((prev) => !prev)}>
+            </i>
             <i style={{color: "white", cursor: "pointer"}} className={`fa-heart ${currentSongData.isLiked ? "fa-solid" : "fa-regular"}`} onClick={() => toggleLike(currentSongData)}></i>
         </div>
     </div>
