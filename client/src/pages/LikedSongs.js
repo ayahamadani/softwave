60476ef974c9a7824ccdabefc8631c264a92fad7
Aya Hamadani <ayahamadani2004@gmail.com>
2025-05-17 15:25:07 +0300

    fix the backend url for the song recommendation route

--- client/src/pages/LikedSongs.js
+++ client/src/pages/LikedSongs.js
@@ -48,7 +48,7 @@ export default function LikedSongs() {
       
      const fetchRecommendations = async () => {
   try {
-    const response = await fetch('http://localhost:5000/recommend', {
+    const response = await fetch('https://softwave-music-player.onrender.com/recommend', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
